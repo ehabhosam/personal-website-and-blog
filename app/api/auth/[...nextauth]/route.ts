4def81ef1f08aec51fdadca96cab517ac71441a2
Ehab Hosam <ehab.hosam2019@gmail.com>
2025-07-27 15:13:12 +0300

    fix a stupid error caused by chmod comment

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
-export { /* @next-codemod-error `GET` export is re-exported. Check if this component uses `params` or `searchParams`*/
-GET, /* @next-codemod-error `POST` export is re-exported. Check if this component uses `params` or `searchParams`*/
-POST } from 'app/auth';
+export {
+    GET,
+    POST
+} from 'app/auth';
+
 export const runtime = 'edge';
